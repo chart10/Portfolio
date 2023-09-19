8cccf132fabc7a906f33eff01133a764b6eee5c4
Christian Hart <christiantaylorhart@gmail.com>
2023-09-19 11:13:09 -0400

    removed App.css, formatting footer src code link

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
 import LandingPage from './Landing/LandingPage';
 import AboutMe from './AboutMe/AboutMe';
 import ProjectsPage from './Projects/ProjectsPage';
--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -1,11 +1,20 @@
 import './footer.css';
-import { BsChatHeart } from 'react-icons/bs';
+import {
+  BsChatHeart,
+  BsBoxArrowInRight,
+  BsArrowReturnRight,
+} from 'react-icons/bs';
 
 const footer = () => {
   return (
     <div className='footer'>
       <p>
         Website designed by me! <BsChatHeart />
+      </p>
+      <p id='website-src'>
+        <a href='https://github.com/chart10/Portfolio' target='_blank'>
+          <BsArrowReturnRight /> Source Code
+        </a>
       </p>
     </div>
   );
