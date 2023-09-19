0b1cb87c0dd6779ea60fad090e0f2e3bcb9202df
Christian Hart <christiantaylorhart@gmail.com>
2023-09-18 21:42:30 -0400

    resume moved to public

--- src/Landing/LandingPage.jsx
+++ src/Landing/LandingPage.jsx
@@ -18,7 +18,7 @@ const LandingPage = () => {
   };
 
   const openResume = () => {
-    window.open('src/assets/ChristianHartResume.pdf', '_blank');
+    window.open('public/ChristianHartResume.pdf', '_blank');
   };
   const openGitHub = () => {
     window.open('https://github.com/chart10', '_blank');
