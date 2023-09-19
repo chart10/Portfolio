d4aa6bc828bda36843094080b1a4349d1ad31399
Christian Hart <christiantaylorhart@gmail.com>
2023-09-19 15:29:29 -0400

    minor changes to title for landing and about me

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -5,6 +5,7 @@ import './aboutMe.css';
     * Write a brief but thorough synopsis of what I'm all about:
       my skills, ambitions, interests and passions
     - Give this page some visual flair
+    - Add a list of tech I've worked with
 */
 
 const AboutMe = () => {
@@ -12,13 +13,21 @@ const AboutMe = () => {
     <div className='page' id='about-me'>
       <div id='about-me-heading'>
         <h4 id='about-me-title'>Hello! I'm Christian</h4>
-        <h5 id='about-me-subtitle'>Developer & Designer</h5>
+        <h5 id='about-me-subtitle'>Designer, Developer & Collaborator</h5>
       </div>
       <div className='info-content'>
         <p>
-          I am a web developer and full stack engineer from Atlanta, Georgia. I
-          love the outdoors, rock climbing, and slaying monsters in the Lands
-          Between.
+          I am a versatile web developer and full stack engineer based in
+          Atlanta, GA, with a strong foundation in JavaScript (primarily with
+          React.js), Java, Python, and MySQL. I thrive in collaborative
+          environments, having successfully contributed to the development of
+          several full-stack web applications within agile teams. My skills
+          extends beyond the code, too. I'm dedicated to creating user-friendly,
+          efficient, innovative, and beautiful web experiences. I’m also an avid
+          learner, always eager to explore new technologies to enhance my skill
+          set. While rooted in Atlanta, I'm open to exciting opportunities in
+          new cities, ready to bring my passion for web development to any
+          project. Let's build something remarkable together!
         </p>
       </div>
     </div>
